f46c1c1a9c93bd8cdcd6d717825d92807a9bd2c1
Casey <c-lambie@hotmail.co.uk>
2016-05-14 20:00:06 +0100

    Basic interface constructed - no functionality.

--- Dedotter/Dedotter/Form1.Designer.cs
+++ Dedotter/Dedotter/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dedotter
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlBrowse = new System.Windows.Forms.Panel();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtWorkingDirectory = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chkDelete = new System.Windows.Forms.CheckBox();
+            this.fbdDirectoryBrowse = new System.Windows.Forms.FolderBrowserDialog();
+            this.pnlBrowse.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBrowse
+            // 
+            this.pnlBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlBrowse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlBrowse.Controls.Add(this.txtWorkingDirectory);
+            this.pnlBrowse.Controls.Add(this.btnBrowse);
+            this.pnlBrowse.Location = new System.Drawing.Point(12, 12);
+            this.pnlBrowse.Name = "pnlBrowse";
+            this.pnlBrowse.Size = new System.Drawing.Size(663, 34);
+            this.pnlBrowse.TabIndex = 0;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Location = new System.Drawing.Point(12, 52);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(663, 286);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(583, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // txtWorkingDirectory
+            // 
+            this.txtWorkingDirectory.Location = new System.Drawing.Point(3, 6);
+            this.txtWorkingDirectory.Name = "txtWorkingDirectory";
+            this.txtWorkingDirectory.Size = new System.Drawing.Size(574, 20);
+            this.txtWorkingDirectory.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(600, 344);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // chkDelete
+            // 
+            this.chkDelete.AutoSize = true;
+            this.chkDelete.Location = new System.Drawing.Point(481, 348);
+            this.chkDelete.Name = "chkDelete";
+            this.chkDelete.Size = new System.Drawing.Size(109, 17);
+            this.chkDelete.TabIndex = 3;
+            this.chkDelete.Text = "Delete, don\'t hide";
+            this.chkDelete.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(687, 379);
+            this.Controls.Add(this.chkDelete);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlBrowse);
+            this.Name = "frmMain";
+            this.Text = "Dedotter";
+            this.pnlBrowse.ResumeLayout(false);
+            this.pnlBrowse.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlBrowse;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtWorkingDirectory;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckBox chkDelete;
+        private System.Windows.Forms.FolderBrowserDialog fbdDirectoryBrowse;
     }
 }
 
--- Dedotter/Dedotter/Form1.cs
+++ Dedotter/Dedotter/Form1.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace Dedotter
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
+
+
     }
 }
